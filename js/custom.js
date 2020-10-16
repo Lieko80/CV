759bfbd9ba023d58a47f7ab24b53b46700a23330
Fabien Trouillet <trouillet.fabien@yahoo.com>
2020-10-16 14:42:28 +0200

    CV V1.8

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	setTimeout(function(){
 		$('body').addClass('loaded');
 		$('h1').css('color','#222222');
-  }, 5000);
+  }, 3000);
   
 //sidebar-------------------------------------------------------------
     $('.sidenav').sidenav();
